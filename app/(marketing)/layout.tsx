aac556fe769b3b1ec1d92e6d58e35d534257ea0a
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-02-19 21:42:19 +0530

    Add Navbar and Footer components to marketing layout

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,12 +1,16 @@
+import { Footer } from './_components/Footer';
+import { Navbar } from './_components/Navbar';
+
 const MarketingLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="h-full bg-sky-100">
       {/* NavBar */}
-
+      <Navbar></Navbar>
       {/* Main Content */}
       <main className="pt-40 pb-20 bg-sky-100">{children}</main>
 
       {/* Footer */}
+      <Footer></Footer>
     </div>
   );
 };
---
+++ todo.md
@@ -0,0 +1,1 @@
+page where all subscriptions are available and easy to manage.
