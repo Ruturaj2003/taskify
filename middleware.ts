53e3e7b8aae0c99fcbc87a437764afcbfa526182
Ruturaj2003 <ruturajhb2003@gmail.com>
2025-02-22 15:43:12 +0530

    Finally setup the middleware

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,35 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from 'next/server';
 
 const isPublicRoute = createRouteMatcher(['/sign-in(.*)', '/sign-up(.*)', '/']);
 
 export default clerkMiddleware(async (auth, request) => {
-  const { userId } = await auth();
+  const { userId, orgId, redirectToSignIn } = await auth();
 
   if (!isPublicRoute(request)) {
     await auth.protect();
   }
 
-  // If it's a public route...
-  if (isPublicRoute(request)) {
-    // ...but the user is signed in, redirect them to /select-org
+  if (userId && isPublicRoute(request)) {
+    let path = '/select-org';
+    if (orgId) {
+      path = `/organization/${orgId}`;
+    }
 
-    // Otherwise, allow access to the public route
-    return;
+    const orgSelection = new URL(path, request.url);
+    return NextResponse.redirect(orgSelection);
+  }
+
+  if (!userId && !isPublicRoute(request)) {
+    return redirectToSignIn({
+      returnBackUrl: request.url,
+    });
+  }
+
+  if (userId && orgId && request.nextUrl.pathname !== '/select-org') {
+    const orgSelection = new URL('/select-org', request.url);
+
+    return NextResponse.redirect(orgSelection);
   }
 });
 
